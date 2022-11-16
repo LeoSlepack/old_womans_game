26ee349d5379ce956cae0d4a50b480d5bfe8b4c7
Leonardo Slepack <111914236+LeoSlepack@users.noreply.github.com>
2022-11-16 14:05:57 -0300

    finish project game

--- js/script.js
+++ js/script.js
@@ -28,6 +28,16 @@ boxes[i].addEventListener("click", function() {
             //Computar a jogada
             if (player1 == player2) {
                 player1++;
+
+                if(secondPlayer == 'ai-players') {
+
+                    //Função para jogada
+                    computerPlay();
+
+                    player2++;
+
+                }
+
             } else {
                 player2++;
             }
@@ -37,6 +47,28 @@ boxes[i].addEventListener("click", function() {
 
         }
 
+
+    });
+
+}
+
+// Evento para saber quem vai jogar
+for (let i = 0; i < buttons.length; i++) {
+
+    buttons[i].addEventListener("click", function() {
+
+        secondPlayer = this.getAttribute("id");
+
+        for (let j =0; j < buttons.length; j++) {
+            buttons[j].style.display = 'none';
+        }
+
+        setTimeout(function() {
+
+            let container = document.querySelector("#container");
+            container.classList.remove("hide");
+
+        }, 500);
 
     });
 
@@ -254,5 +286,38 @@ function checkEl(player1, player2) {
         for (let i = 0; i < boxesToRemove.length; i++) {
             boxesToRemove[i].parentNode.removeChild(boxesToRemove[i]);
         }
+    }
+
+    // Executar a lógica da jogada do cpu
+    function computerPlay() {
+
+        let cloneO = o.cloneNode(true);
+        counter = 0;
+        filled = 0;
+
+        for (let i= 0; i < boxes.length; i++) {
+
+            let randomNumber = Math.floor(Math.random() * 5);
+
+
+            // Só vai preencher se estiver vazio o filho
+            if (boxes[i].childNodes[0] == undefined) {
+                if(randomNumber <= 1) {
+                    boxes[i].appendChild(cloneO);
+                    counter++;
+                    break;
+                }
+            // check de quantas estão preenchidas
+            } else {
+                filled++;
+            }
+
+        }
+
+        if (counter == 0 && filled < 9) {
+            computerPlay();
+        }
+
+
     }
 
